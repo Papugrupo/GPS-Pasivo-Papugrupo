2f53357964265c1cd4db2380fcecf2ee1448ffbc
Carlos Jara <89615893+konungrDRAGO@users.noreply.github.com>
2025-05-03 16:17:45 -0400

    Fix(axiosPublic): se hace que los metodos get no tengan header

--- frontend/src/api/axiosPublic.js
+++ frontend/src/api/axiosPublic.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 const axiosPublic = axios.create({
   baseURL: import.meta.env.VITE_API_BACKEND,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+});
+
+axiosPublic.interceptors.request.use(config => {
+  if (['post', 'put', 'patch'].includes(config.method)) {
+    config.headers['Content-Type'] = 'application/json';
+  } else {
+    delete config.headers['Content-Type'];
+  }
+  return config;
 });
 
 export default axiosPublic;
