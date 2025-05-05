bda80e8eeef91cfa3ec589536535781690a764b8
Yanko Acuña <yacuna20@alumnos.utalca.cl>
2025-05-04 22:49:37 -0400

    feat(fix): Correccion de botones de zoom y enlace a login

--- frontend/src/pages/MapaMascota.jsx
+++ frontend/src/pages/MapaMascota.jsx
@@ -242,7 +242,7 @@ const MapaMascota = () => {
                 </div>
                 
                 {/* Controles de zoom */}
-                <div className="flex space-x-2">
+                {/*<div className="flex space-x-2">
                   <button 
                     onClick={handleZoomOut}
                     className="bg-white p-2 rounded-md shadow hover:bg-gray-100"
@@ -261,7 +261,7 @@ const MapaMascota = () => {
                       <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
                     </svg>
                   </button>
-                </div>
+                </div>*/}
               </div>
 
               {/* Tabla de ubicaciones */}
